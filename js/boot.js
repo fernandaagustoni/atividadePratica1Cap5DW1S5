314e1033b60d9dc23a5053b7abdf5bb143e8dd2f
fernandaagustoni <fernanda_agustoni@hotmail.com>
2023-04-16 13:06:09 -0300

    feat: configure load

--- js/boot.js
+++ js/boot.js
@@ -1,1 +1,13 @@
-var bootState = {};
+
+//Inicia o jogo
+var bootState = {
+    preload: function(){
+        game.load.image('progressBar', 'img/progressBar.png');
+    },
+
+    //Chama o load.js
+    create: function(){
+        game.state.start('load');
+    }
+};
+
--- js/load.js
+++ js/load.js
@@ -1,1 +1,26 @@
-var loadState = {};
+var loadState = {
+    preload: function(){
+        var progressBar = game.add.sprite(game.world.centerX,250, 'progressBar');
+        var txtLoading = game.add.text(game.world.centerX, 200, 'LOADING...', {font:'15px Arial', fill:'#fff'});
+        progressBar.anchor.set(.5);
+        txtLoading.anchor.set(.5);
+    
+        game.load.setPreloadSprite(progressBar);
+        
+        //Carregamento de imagens
+        game.load.image('bg', 'img/bg.png');
+        game.load.image('block', 'img/block.png');
+        game.load.image('end', 'img/end1.png');
+        game.load.image('part', 'img/part.png');
+        
+        //Carregamento de Spritesheet
+        game.load.spritesheet('coin', 'img/coin.png', 32,32);
+        game.load.spritesheet('enemy', 'img/enemy1.png', 150,90);
+        game.load.spritesheet('player', 'img/player1.png', 49,64);
+
+        //Carregamento dos audios
+        game.load.audio('getitem', 'songs/getitem.ogg');
+        game.load.audio('loseitem', 'songs/loseitem.ogg');
+        game.load.audio('music', 'songs/music.ogg');
+    }
+};
